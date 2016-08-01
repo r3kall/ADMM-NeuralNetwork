aac5f1ebbb18ea5e6b33f93882d4a9fe9f2edabd
r3kall <lnz.rutigliano@gmail.com>
2016-08-01 20:23:50 +0200

    layers: train, fixes, loss_function

--- admm-nn/auxiliaries.py
+++ admm-nn/auxiliaries.py
@@ -7,19 +7,6 @@ __author__ = 'Lorenzo Rutigliano, lnz.rutigliano@gmail.com'
 log = defineLogger(Loggers.STANDARD)
 
 
-def relu(x):
-    return np.maximum(0, x)
-
-
-def print_limited_matrix(x, n, m):
-    # da migliorare, meglio 'prin_matrix_info', in cui usare 'check_type'
-    # if check_type ok, se matrice get_matrix_info, oppure get_vector_info
-    # if n <= 1 or n > x.dim or None raise
-    for i in range(n):
-        for j in range(m):
-            print(x[i][j])
-
-
 def check_consistency(a):
     if a is None:
         raise TypeError("Invalid type: None")
@@ -37,3 +24,22 @@ def check_dimensions(a, n, m):
     if a.shape[0] != n or a.shape[1] != m:
         raise ValueError("Invalid dimensions of the array")
     log.debug("Function '%s' validates the array" % check_dimensions.__name__)
+
+
+def relu(x):
+    return np.maximum(0, x)
+
+
+def hinge_loss(prediction, label):
+    num = 0.
+    dem = 0.
+    if len(prediction) == len(label):
+        for i in range(len(prediction)):
+            prod = prediction[i] * label[i]
+            num += max(0, 1 - prod)
+            dem += 1
+        return num/dem
+    raise ValueError("Array dimensions don't match"
+                     "\nPrediction length: %s"
+                     "\nLabel length: %s" % (len(prediction), len(label))
+                     )
