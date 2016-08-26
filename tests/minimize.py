267cee65e7197e4f0821d1da514c72e62065e85b
r3kall <lnz.rutigliano@gmail.com>
2016-08-26 21:33:34 +0200

    # minimization of z

--- tests/minimize.py
+++ tests/minimize.py
@@ -12,26 +12,27 @@ import auxiliaries
 
 __author__ = "Lorenzo Rutigliano, lnz.rutigliano@gmail.com"
 
+
 def relu(x):
     return np.maximum(0, x)
 
+
 def min(a, m, alpha, beta):
+    if a <= 0 and m <= 0:
+        return m
     sol = ((alpha * a) + (beta * m)) / (alpha + beta)
     if a >= 0 and m >= 0:
         return sol
-    elif a <= 0 and m <= 0:
-        return m
-    print(sol)
-    print(m)
-    res1 = alpha * ((a - relu(sol)) ** 2) + beta * ((relu(sol) - m) ** 2)
-    res2 = alpha * (a - relu(m)) ** 2
-    print(res1)
-    print(res2)
-    if res1 < res2:
+    if m < 0 < a:
+        if sol / (a ** 2) > 1:
+            return sol
+        else:
+            return m
+    if a < 0 < m:
         return sol
-    return m
 
 
 def test_1():
     print()
-    min(0.5, -0.6, 10, 1)
+    for i in range(1000*10000):
+        res = min(0.5, -0.5, 10, 1)
