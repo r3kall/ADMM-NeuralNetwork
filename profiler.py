133e72ddc97e1b5b123c42c43f3889a86648900a
r3kall <lnz.rutigliano@gmail.com>
2016-10-08 22:25:59 +0200

    # iris reporting

--- profiler.py
+++ profiler.py
@@ -51,16 +51,14 @@ def get_data():
     return trn, tst
 
 
-def get_iris():
+def get_iris(rng=42, tst_size=0.3):
     iris = datasets.load_iris()
     X = iris.data
     y = iris.target
 
-    X = rep(X, 8)
-    y = rep(y, 8)
-
-    rng = 42
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=rng)
+    X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                        test_size=tst_size,
+                                                        random_state=rng)
 
     trg_train = np.zeros((3, len(y_train)), dtype='uint8')
     for e in range(trg_train.shape[1]):
@@ -77,11 +75,8 @@ def get_iris():
     return trn, tst
 
 
-def rep(matrix, times):
-    assert times > 0
-    for i in range(times):
-        matrix = np.r_[matrix, matrix]
-    return matrix
+def column_normalisation(x):
+    pass
 
 
 def rnd_train(net, trn_instance, train_iters=1, warm_iters=0):
@@ -92,7 +87,7 @@ def rnd_train(net, trn_instance, train_iters=1, warm_iters=0):
     for i in range(train_iters):
         # Standard Training
         net.train(trn_instance.samples, trn_instance.targets)
-    endt = np.round((time.time() - st), decimals=2)
+    endt = np.round((time.time() - st), decimals=6)
     return net, endt
 
 from src.commons import get_max_index, convert_binary_to_number
@@ -108,7 +103,7 @@ def rnd_test(net, tst_instance):
         if output == label:
             c += 1
     approx = float(c) / float(test)
-    return np.round(approx, decimals=5)
+    return np.round(approx, decimals=6)
 
 
 def rnd_measure(accuracy, ws, m=10, k=30):
@@ -207,29 +202,24 @@ def comp_lambda_target(l, y):
     return counter
 
 
-def iris_measure(accuracy, ws, m=10, k=30):
-    trn, tst = get_iris()
+def iris_measure(trn, tst, ws, m=10, k=100):
+    res = []
 
-    print(trn.samples.shape)
-    print(trn.targets.shape)
-    print(tst.samples.shape)
-    print(tst.targets.shape)
-
-    rundict = {
-        'runc': [0 for p in range(k)],
-        'runover': 0,
-        'timec': 0.
-    }
+    class rundict():
+        def __init__(self, accuracylabel, timelabel, runsnumber):
+            self.accuracy = accuracylabel
+            self.time = timelabel
+            self.run = runsnumber
 
     for it in range(m):
         net = NeuralNetwork(trn.samples.shape[1],
                             trn.samples.shape[0],
                             trn.targets.shape[0],
-                            64, gamma=10., beta=1.)
+                            23, gamma=1., beta=0.5)
 
         flag = False
         ttmp = 0.
-        print("============ " + str(it + 1))
+        tmpacc = -1.
 
         for innit in range(k):
             if flag is False:
@@ -237,47 +227,28 @@ def iris_measure(accuracy, ws, m=10, k=30):
                 acc = rnd_test(net, tst)
                 flag = True
                 ttmp += t
-            mean = mean_lambda(net.l)
-            if acc < accuracy:
+
+            if acc < 0.99:
                 net, t = rnd_train(net, trn, train_iters=1, warm_iters=0)
                 acc = rnd_test(net, tst)
-                ttmp += t
+                if tmpacc >= acc:
+                    res.append(rundict(tmpacc, ttmp, innit))
+                    break
+                tmpacc = acc
             else:
-                rundict['runc'][innit] += 1
-                rundict['timec'] += ttmp
+                res.append(rundict(acc, ttmp, innit + 1))
                 break
-            """
-            if mean_lambda(net.l) - mean < 0.000000001:
-                print(mean_lambda(net.l))
-                print(mean)
-                print(acc)
-                break
-            """
             if innit == k - 1:
-                rundict['runover'] += 1
-                print("Reached Accuracy: %s" % str(acc))
-                e = mean_lambda(net.l)
-                print("Lambda mean: %s" % str(e))
-            # elif innit % 5 == 0:
-                # print("Accuracy at %s: %s" % (str(innit), str(acc)))
-
-    overs = rundict['runover'] / m
-    runs = [x / m for x in rundict['runc']]
-    if m > rundict['runover']:
-        times = rundict['timec'] / (m - rundict['runover'])
-    else:
-        times = 0
-    print(runs)
-    print(overs)
-    return runs, overs, times
+                res.append(rundict(acc, ttmp, innit + 1))
+    return res
 
 
 def main_classification():
     import operator
     p = []
-    a = [0.80, 0.85]
+    a = [0.85]
     for i in a:
-        runs, over, times = iris_measure(i, 6, k=100)
+        runs, over, times = iris_measure(i, 1, k=100)
         if times == 0:
             continue
         else:
@@ -288,5 +259,66 @@ def main_classification():
     # plotout(newlist)
 
 
+def main_iris():
+
+    def comp_iris(ming, maxg, mg, sp=0.3):
+        mean_acc = 0.
+        mean_time = 0.
+        mean_runs = 0.
+        min_acc = 0.
+        max_acc = 0.
+        delta = maxg - ming
+        for i in range(ming, maxg):
+            trn, tst = get_iris(rng=i, tst_size=sp)
+            res = iris_measure(trn, tst, 1, m=mg)
+            mean_acc += np.mean([e.accuracy for e in res])
+            mean_time += np.mean([e.time for e in res])
+            mean_runs += np.mean([e.run for e in res])
+            min_acc += np.min([e.accuracy for e in res])
+            max_acc += np.max([e.accuracy for e in res])
+        print(
+            "mean accuracy: %f   min peak: %f   max peak: %f   mean time: %f   mean runs: %f" %
+            (mean_acc / delta, min_acc / delta, max_acc / delta, mean_time / delta, mean_runs / delta))
+
+    print("===================================================================" * 2)
+    print("Compare one execution with two different splitting of the dataset")
+    trn, tst = get_iris(rng=11)
+    res2 = iris_measure(trn, tst, 1, m=1)
+    trn, tst = get_iris(rng=42)
+    res1 = iris_measure(trn, tst, 1, m=1)
+    print("rng: %d   accuracy: %f   time: %f   runs: %d" % (42, res1[0].accuracy, res1[0].time, res1[0].run))
+    print("rng: %d   accuracy: %f   time: %f   runs: %d" % (11, res2[0].accuracy, res2[0].time, res2[0].run))
+    print("===================================================================" * 2)
+    print("Compare multiple executions of the same splitting (rng = 42)")
+    comp_iris(42, 43, 1000)
+    print("===================================================================" * 2)
+    print("Compare multiple executions of different splitting of the dataset   [0 <= rng < 10]")
+    comp_iris(0, 10, 100)
+    print("===================================================================" * 2)
+    print("Compare multiple executions of different splitting of the dataset", end="")
+    print("   [random <= rng < random + 100]")
+
+    g = 1 + np.random.randint(1000) + (np.random.randint(20) * np.random.randint(51))
+    print("random = %s" % str(g))
+    comp_iris(g, g + 100, 100)
+    print("===================================================================" * 2)
+    print("Compare multiple executions of different splitting of the dataset", end="")
+    print("   [random <= rng < random + 100]", end="")
+    print("   [0.1 <= test size <= 0.5]")
+    g = 1 + np.random.randint(1000) + (np.random.randint(20) * np.random.randint(51))
+    print("random = %s" % str(g))
+    print("test size: %s   " % str(0.1), end="")
+    comp_iris(g, g + 100, 100, sp=0.1)
+    print("test size: %s   " % str(0.2), end="")
+    comp_iris(g, g + 100, 100, sp=0.2)
+    print("test size: %s   " % str(0.3), end="")
+    comp_iris(g, g + 100, 100, sp=0.3)
+    print("test size: %s   " % str(0.4), end="")
+    comp_iris(g, g + 100, 100, sp=0.4)
+    print("test size: %s   " % str(0.5), end="")
+    comp_iris(g, g + 100, 100, sp=0.5)
+    print("===================================================================" * 2)
+
+
 if __name__ == '__main__':
-    main_classification()
+    main_iris()
